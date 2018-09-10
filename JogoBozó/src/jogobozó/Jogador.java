3752fdd9f4756b83fc1645a2eb71ba2ef218a653
ice <ice@lab06m02.nrc.ice.ufjf.br>
2018-09-10 20:55:25 -0300

    Bozó

--- JogoBozó/src/jogobozó/Jogador.java
+++ JogoBozó/src/jogobozó/Jogador.java
@@ -2,19 +2,72 @@ package jogobozó;
 import java.util.Scanner;
 public class Jogador {
     String nome;
-    int [] dadoo;
-    Jogador(){
-        this.dadoo = new int[5];
+    int [] cincoDados;
+    Jogador(String nome){
+    this.nome=nome;
+    cincoDados=new int [5];
     }
-    void jogaDado(Dado dado1, Dado dado2, Dado dado3, Dado dado4, Dado dado5){
-        dadoo[0]=dado1.retornaFace();
-        dadoo[1]=dado2.retornaFace();
-        dadoo[2]=dado3.retornaFace();
-        dadoo[3]=dado4.retornaFace();
-        dadoo[4]=dado5.retornaFace();
+    void jogaDado(Dado [] dados){
+        for (int i=0; i<5; i++){
+            cincoDados[i]=dados[i].jogarDado();
+            System.out.println(cincoDados[i]);
+        }
     }
-
-    void voltaDado(Dado dado){
+    
+    void palpite(){
+        System.out.println("Escolha a sua jogada \n AS(1) - DUQUE(2) - TERNA(3) - QUADRA(4) - QUINA(5) - SENA(6) - FU(7) - SEGUIDA(8) - QUADRADA(9) - GENERAL(10)");
+        Scanner input = new Scanner(System.in);
+        int palpite = input.nextInt();
+        int p1=0,p2=0,p3=0,p4=0,p5=0,p6=0,p7=0,p8=0,p9=0,p10=0;
+        if(palpite==1){
+            for(int i=0;i<5;i++){
+                if(cincoDados[i]==1){
+                    p1=cincoDados[i]+p1;
+                }
+            }
+        }
+        else if(palpite==2){
+                for(int i=0;i<5;i++){
+                    if(cincoDados[i]==2){
+                    p2=cincoDados[i]+p2;
+                    }
+                }
+        }
+        else if(palpite==3){
+                for(int i=0;i<5;i++){
+                    if(cincoDados[i]==3){
+                    p3=cincoDados[i]+p3;
+                    }
+                }        
+        }
+        else if(palpite==4){
+                for(int i=0;i<5;i++){
+                    if(cincoDados[i]==4){
+                    p4=cincoDados[i]+p4;
+                    }
+                }        
+        }
+        else if(palpite==5){
+                for(int i=0;i<5;i++){
+                    if(cincoDados[i]==5){
+                    p5=cincoDados[i]+p5;
+                    }
+                }        
+        }
+        else if(palpite==6){
+                for(int i=0;i<5;i++){
+                    if(cincoDados[i]==6){
+                    p6=cincoDados[i]+p6;
+                    }
+                }        
+        }
+        else if(palpite==7){}
+        else if(palpite==8){}
+        else if(palpite==9){}
+        else if(palpite==10){}
+    }
+    
+    void voltaDado(Dado [] dados){
         Scanner reader = new Scanner(System.in);
         System.out.println("Deseja trocar quantos dados?0-5");
         int r = reader.nextInt();
@@ -24,9 +77,10 @@ public class Jogador {
         else{
             for(int i=0;i<r;i++){
                 Scanner ler = new Scanner(System.in);
-                System.out.println("Deseja trocar qual dado?0-5");
+                System.out.println("Deseja trocar qual dado?0(+1)-4(+1)");
                 int d = reader.nextInt();
-                dadoo[d]=dado.jogarDado();
+                cincoDados[d]=dados[d].jogarDado();
+                System.out.println("Novo dado:"+cincoDados[d]);
             }    
         }
     }
